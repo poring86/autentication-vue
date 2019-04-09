66adabae594e79db7636b957670986f03a87e15d
Matheus <matheuslino86a@gmail.com>
2019-04-08 23:08:36 -0400

    autologin

--- src/store.js
+++ src/store.js
@@ -27,7 +27,7 @@ export default new Vuex.Store({
     setLogoutTimer({commit}, expirationTime){
       setTimeout(() => {
         commit('clearAuthData')
-      },expirationTime)
+      },expirationTime * 1000)
     },
     signup({commit, dispatch}, authData){
       axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAoMj7f0KW_3SyLvvmohqRbGDAFGSbeKPo', {
@@ -40,6 +40,11 @@ export default new Vuex.Store({
           token: res.data.idToken,
           userId: res.data.localId
         })
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+        localStorage.setItem('token', res.data.idToken)
+        localStorage.setItem('userId', res.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
         dispatch('storeUser', authData)
         dispatch('setLogoutTimer', res.data.expiresIn)
       })
@@ -52,6 +57,11 @@ export default new Vuex.Store({
         returnSecureToken: true
       })
       .then(res =>{
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+        localStorage.setItem('token', res.data.idToken)
+        localStorage.setItem('userId', res.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
         commit('authUser', {
           token: res.data.idToken,
           userId: res.data.localId
@@ -60,8 +70,27 @@ export default new Vuex.Store({
       })
       .catch(error => console.log(error))
     },
+    tryAutoLogin({commit}){
+      const token = localStorage.getItem('token')
+      if(!token){
+        return
+      }
+      const expirationDate = localStorage.getItem('expirationDate')
+      const now = new Date()
+      if(now <= expirationDate){
+        return
+      }
+      const userId = localStorage.getItem('userId')
+      commit('authUser', {
+        token: token,
+        userId: userId
+      })
+    },
     logout({commit}){
       commit('clearAuthData')
+      localStorage.removeItem('expirationDate')
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
       router.replace('/signin')
     },
     storeUser({commit, state}, userData){
